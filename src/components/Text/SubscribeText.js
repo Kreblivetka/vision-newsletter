1571e18ce4372be87237105df44b65cf74732550
Jana <lucyfairy10@gmail.com>
2023-10-20 13:25:24 +0300

    created first component for subscribe text

---
+++ src/components/Text/SubscribeText.js
@@ -0,0 +1,11 @@
+import "./SubscribeText.css";
+
+function SubscribeText() {
+  return (
+    <div className="text">
+      <h1>Subscribe to newsletter</h1>
+      <p>Subscribe to our newsletter and get 10% discount on pineapple glass.</p>
+    </div>
+  );
+}
+export default SubscribeText;
